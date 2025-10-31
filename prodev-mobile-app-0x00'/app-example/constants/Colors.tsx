c7236f398e389974599085ed4964373dcd14fd54
olivier01A <oliviermasengesho590@gmail.com>
2025-10-31 15:58:36 +0100

    Create Colors.tsx

---
+++ prodev-mobile-app-0x00'/app-example/constants/Colors.tsx
@@ -0,0 +1,22 @@
+// app-example/constants/Colors.tsx
+
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#888',
+    tabIconSelected: tintColorDark,
+  },
+};
+
